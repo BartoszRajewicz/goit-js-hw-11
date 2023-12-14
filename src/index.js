c85228da6c806b9f1dd1495ea4303aa17f758496
BartoszRajewicz <szejn3@gmail.com>
2023-12-14 22:31:16 +0100

    feat: added selectors and listeners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import { fetchImages } from "./pixabay-api";
-import Notiflix from "notiflix";
+import { fetchImages } from './pixabay-api';
+import Notiflix from 'notiflix';
 
+const searchForm = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreButton = document.querySelector('.load-more');
+
+let currentPage = 1;
+let currentQuery = '';
+
+searchForm.addEventListener('submit', handleFormSubmit);
+loadMoreButton.addEventListener('click', loadMoreImages);
