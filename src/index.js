54b099b16898140b676d8909accdae8e6224bb77
BartoszRajewicz <szejn3@gmail.com>
2023-12-14 22:47:04 +0100

    feat: added handleFormSubmit

--- src/index.js
+++ src/index.js
@@ -10,3 +10,31 @@ let currentQuery = '';
 
 searchForm.addEventListener('submit', handleFormSubmit);
 loadMoreButton.addEventListener('click', loadMoreImages);
+
+async function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  const seachQuery = formData.get('seachQuery').trim();
+
+  if (seachQuery !== currentQuery) {
+    clearGallery();
+    currentPage = 1;
+    currentQuery = searchQuery;
+  }
+  try {
+    const images = await fetchImages(searchQuery, currentPage);
+
+    if (images.length > 0) {
+      renderImages(images);
+      showLoadMoreButton();
+    } else {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      hideLoadMoreButton();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
